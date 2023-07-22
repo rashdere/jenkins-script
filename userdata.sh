06a4bd8e24e5848eeda1e489eb7a313704098973
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-07-22 09:08:18 +0100

    modified userdata

--- userdata.sh
+++ userdata.sh
@@ -9,13 +9,17 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update -y 
-
 sudo apt install openjdk-17-jre -y 
-
 sudo apt-get install jenkins -y
 
+
 apt-get update -y
-
 apt-get install python3.8 -y
-
 apt-get install -y python3-pip
+
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update && sudo apt install terraform -y
