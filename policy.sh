20ced85ae1079167988df7d3baa2c0f555afc8f9
rashdere <145078413+rashdere@users.noreply.github.com>
2023-12-02 10:26:04 -0400

    Update policy.sh

--- policy.sh
+++ policy.sh
@@ -5,7 +5,7 @@
             "Sid": "SSMGetParameter",
             "Effect": "Allow",
             "Action": "ssm:GetParameter",
-            "Resource": "arn:aws:ssm:eu-west-2::parameter/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+            "Resource": "arn:aws:ssm:us-east-1::parameter/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
         }
     ]
 }
